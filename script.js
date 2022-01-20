d587b8124d97e955e6b04082dead7780bcae4c64
Roxanne Weber <marie.sankofa@gmail.com>
2022-01-20 16:24:58 -0600

    updating all scripts

---
+++ script.js
@@ -0,0 +1,85 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionairesBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// fetch random user and add money
+async function getRandomUser() {
+	const res = await fetch('https://randomuser.me/api');
+	const data = await res.json();
+
+	const user = data.results[0];
+
+	const newUser = {
+		name: `${user.name.first} ${user.name.last}`,
+		money: Math.floor(Math.random() * 1000000000),
+	};
+	addData(newUser);
+}
+
+// double everyones money
+function doubleMoney() {
+	data = data.map((user) => {
+		return { ...user, money: user.money * 2 };
+	});
+	updateDOM();
+}
+
+// sort by richest
+
+function sortByRichest() {
+	data.sort((a, b) => b.money - a.money);
+	updateDOM();
+}
+
+// filter only millionaires
+function showMillionaires() {
+	data = data.filter((person) => person.money > 1000000000);
+	updateDOM();
+}
+
+// calculate total value
+
+// add newUser data to (obj) to the data array
+function addData(obj) {
+	data.push(obj);
+
+	updateDOM();
+}
+
+// update the DOM
+function updateDOM(providedData = data) {
+	// clear main div
+	main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+	providedData.forEach((item) => {
+		const element = document.createElement('div');
+		element.classList.add('person');
+		element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+			item.money
+		)}`;
+		main.appendChild(element);
+	});
+}
+
+// format number as money
+function formatMoney(number) {
+	return '$ ' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+// event listeners
+addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
+
+showMillionairesBtn.addEventListener('click', showMillionaires);
+
+sortBtn.addEventListener('click', sortByRichest);
+// calculateWealthBtn.addEventListener('click');
